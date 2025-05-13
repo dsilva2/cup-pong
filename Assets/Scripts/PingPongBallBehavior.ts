d05766504afe07b1cf9be2649c4b1b0c8bd47a0e
dsilva2 <dsilva24@stanford.edu>
2025-05-13 14:44:46 -0700

    regenerate and sync ball

--- Assets/Scripts/PingPongBallBehavior.ts
+++ Assets/Scripts/PingPongBallBehavior.ts
@@ -11,13 +11,15 @@ export class PingPongBallBehavior extends TennisBallBehavior {
   protected OBJECT_MASS = 0.02;
   protected HAND_ACCELERATION_MULTIPLIER = 0.08;
   protected HAND_BASE_VELOCITY_MULTIPLIER = 0.6;
+  private originPoint: vec3;
+  private distanceThreshold = 150; // Distance in units before regeneration
 
   onAwake() {
     this.audio.playbackMode = Audio.PlaybackMode.LowLatency;
 
     this.sceneObject.getComponent("MeshVisual").mainMaterial = this.sceneObject
       .getComponent("MeshVisual")
-      .mainMaterial.clone(); 
+      .mainMaterial.clone();
     this.sceneObject.getComponent(
       "MeshVisual"
     ).mainMaterial.mainPass.mainColor = this.getRandomRainbowColor();
@@ -38,6 +40,34 @@ export class PingPongBallBehavior extends TennisBallBehavior {
     this.createEvent("UpdateEvent").bind(this.onUpdate.bind(this));
 
     this.t = this.getTransform();
+    this.originPoint = this.t.getWorldPosition();
+  }
+
+  onUpdate() {
+    // Check if ball has strayed too far from origin
+    const currentPos = this.t.getWorldPosition();
+    const distanceFromOrigin = currentPos.distance(this.originPoint);
+
+    if (distanceFromOrigin > this.distanceThreshold) {
+      this.regenerateBall();
+    }
+  }
+
+  private regenerateBall() {
+    // Reset position to origin
+    this.t.setWorldPosition(this.originPoint);
+
+    // Reset physics and disable physics
+    if (this.physicsBody) {
+      this.physicsBody.velocity = new vec3(0, 0, 0);
+      this.physicsBody.angularVelocity = new vec3(0, 0, 0);
+      this.physicsBody.dynamic = false; // Disable physics
+    }
+
+    // Change color for visual feedback
+    this.sceneObject.getComponent(
+      "MeshVisual"
+    ).mainMaterial.mainPass.mainColor = this.getRandomRainbowColor();
   }
 
   getRandomRainbowColor(): vec4 {
@@ -57,5 +87,13 @@ export class PingPongBallBehavior extends TennisBallBehavior {
 
     // Return the selected random color
     return rainbowColors[randomIndex];
+  }
+
+  onTriggerEnd() {
+    // Re-enable physics when the ball is released
+    if (this.physicsBody) {
+      this.physicsBody.dynamic = true; // Re-enable physics
+    }
+    super.onTriggerEnd();
   }
 }
